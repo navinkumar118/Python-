05a02f6d968c6a1f7e406eaf7cfe779b96211c78
navinkumar118 <152601080+navinkumar118@users.noreply.github.com>
2023-12-01 15:02:25 +0530

    Python task 1and task 2

---
+++ main.py
@@ -0,0 +1,13 @@
+#task 1
+print("Hello World")
+
+#task 2
+name="Navinkumar"
+number=0118
+number1=06.6
+print(name,type(name))
+print(id(name))
+print(number,type(number))
+print(id(number))
+print(number1,type(number1))
+print(id(number1))
